0420edb86d667005dd8535b883449cf10067fa4b
eediallo <elhhabdouldiallo@gmail.com>
2025-10-16 12:54:54 +0100

    Solve print day of the week

--- src/Loops/challenges/SwitchChallenge.java
+++ src/Loops/challenges/SwitchChallenge.java
@@ -19,10 +19,26 @@ public class SwitchChallenge {
 
     }
 
+
+    static void printDayOfWeek(int day) {
+        String dayOfWeek = switch (day) {
+            case 0 -> "Sunday";
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid day";
+        };
+
+        System.out.println(day + " stands for " + dayOfWeek);
+    }
+
     static void main() {
         char character = 'e';
         System.out.println(getNatoAlphabet(character));
-
+        printDayOfWeek(5);
 
     }
 }
